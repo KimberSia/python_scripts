c626f037919d2ec6e7a13e0899985940d3aba244
Kimber.Sia <Kimsia478@gmail.com>
2023-02-18 15:17:58 +0000

    update

--- project14.py
+++ project14.py
@@ -1,8 +1,8 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("8714c1f2-af9d-11ed-9e0d-325096b39f47")
+bucket=aws_resource.Bucket("3a1b9398-af9e-11ed-beb7-325096b39f47")
 response = bucket.create(
-    ACL='public-read',
+    ACL='private',
     CreateBucketConfiguration={
         'LocationConstraint':'us-east-2'
         },
