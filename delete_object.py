3418be9ae9939987ea886a888b665531eb722fce
Kimber.Sia <Kimsia478@gmail.com>
2023-02-18 22:43:01 +0000

    deleting multiple objects

--- delete_object.py
+++ delete_object.py
@@ -1,8 +1,10 @@
 #deleting objects 
 
 import boto3
+import os
 
-s3_resource=boto3.client("s3")
-#delete single object
-s3_resource.delete_object(Bucket='5b1988ae-af9c-11ed-b434-325096b39f47',
-    Key='uploadtest2.png')
+s3_resource = boto3.client("s3")
+objects = s3_resource.list_objects(Bucket = "5b1988ae-af9c-11ed-b434-325096b39f47")["Contents"]
+
+for object in objects:
+    response = s3_resource.delete_object(Bucket = "5b1988ae-af9c-11ed-b434-325096b39f47", Key = object["Key"])
