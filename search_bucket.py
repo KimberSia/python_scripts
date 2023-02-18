da62de673b9a3b2c4417f1ef469efd71ada5e414
Kimber.Sia <Kimsia478@gmail.com>
2023-02-18 15:39:19 +0000

    update to #

--- search_bucket.py
+++ search_bucket.py
@@ -3,10 +3,10 @@ import boto3
 
 resource = boto3.resource("s3")
 
-# To use len, create variable of bucket list, then call len on it
+# obtain the number of buckets 
 bucket_list = list(resource.buckets.all())
 print(len(bucket_list))
 
-# To list out all of the buckets
+#list of buckets
 for bucket in resource.buckets.all():
     print(bucket.name)
