b83d8b01d60428f78771f4c85ed44a24b3238022
Kimber.Sia <Kimsia478@gmail.com>
2023-02-19 03:07:09 +0000

    multiple instance

--- create_multiple_instance.py
+++ create_multiple_instance.py
@@ -2,3 +2,12 @@
 
 
 import boto3
+ec2_resource=boto3.resource("ec2")
+
+
+ec2_resource.create_instances(ImageId='ami-0dfcb1ef8550277af',
+   InstanceType='t2.micro',
+   MaxCount=3,
+   MinCount=2)
+
+
