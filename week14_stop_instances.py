cfed784a5713b54d5e61d1dad722f31ed24783cd
KimberSia <kimsia478@gmail.com>
2023-02-20 13:53:36 +0000

    stop instances

---
+++ week14_stop_instances.py
@@ -0,0 +1,11 @@
+
+
+import boto3
+ec2_resource=boto3.resource("ec2")
+
+
+ec2_resource.Instance('i-0d5da7d084b4ddf64').stop()
+ec2_resource.Instance('i-0c0a972270353ab98').stop()
+ec2_resource.Instance('i-05a149fd1aa7682ed').stop()
+
+
