3338701fc0c254f0ad97b88ec6e1a02bf5e23745
KimberSia <kimsia478@gmail.com>
2023-02-26 03:03:27 +0000

    project

--- projectweek15.py
+++ projectweek15.py
@@ -2,9 +2,15 @@
 
 import boto3 
 
+#service resource
 sqs_client = boto3.client("sqs")
-response = sqs_client.create_queue(
-        QueueName="ProjectWeek15"
 
+
+#create the queue
+response = sqs_client.create_queue(
+        QueueName ='ProjectWeek15'
+      
     )
+#prints the queue url along additional information    
 print(response)
+
